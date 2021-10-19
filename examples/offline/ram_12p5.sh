c8b73e2ab9d3294c2ab08c1415539d2b277b1326
Zachary S. Rohde <zach.rohde@jhuapl.edu>
2021-10-19 15:26:56 -0700

    Created a realtime test script to run a HiL test and renamed offline test to help differentiate scripts

---
+++ examples/offline/ram_12p5.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+#
+# Off-line processing of a recording (12.5 MHz) of HF obtained using thor.py
+
+# ensure the RAM storage is set, if not use:
+#   mount -o remount,size=32G /dev/shm
+
+# program configuration
+CONF_FILE=examples/offline/ram_12p5.ini
+
+# how many CPU cores to use (can be all since this is the only program running)
+N_CPUS=6
+# we use 2 cores for each mpi process when calculating an ionogram
+N_CPUS_I=`expr $N_CPUS / 2`
+
+# spawn the program scripts
+mpirun -np $N_CPUS_I python3 calc_ionograms.py $CONF_FILE >logs/calc_ionograms.log 2>&1 &
+python3 find_timings.py $CONF_FILE >logs/find_timings.log 2>&1 &
+mpirun -np $N_CPUS python3 detect_chirps.py $CONF_FILE >logs/detect_chirps.log 2>&1 &
+python3 plot_ionograms.py $CONF_FILE >logs/plot_ionograms.log 2>&1 &
+
+# processes can be killed using:
+#   pkill -f python3
---
+++ examples/ringbuffer/ram_25.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+#
+# Realtime processing of a recording (25 MHz) of HF obtained using thor.py
+
+# ensure the RAM storage is set, if not use:
+#   mount -o remount,size=RINGBUFFER_SIZE /dev/shm
+
+# program configuration
+SAMPLE_RATE=25e6
+CENTER_FREQ=12.5e6
+CONF_FILE=examples/ringbuffer/ram_25.ini
+
+# path to the digital RF python tool Thor
+THOR_PGRM_DIR=/home/alex/git/digital_rf/python/tools
+
+# ram disk buffer for fast i/o
+RINGBUFFER_DIR=/dev/shm/hf25
+# make this about a few GB more than a full sounder (25 GB)
+RINGBUFFER_SIZE=27GB
+
+# start receiving to a ringbuffer
+rm -Rf $RINGBUFFER_DIR
+python3 $THOR_PGRM_DIR/thor.py -m 192.168.10.2 -d A:A -c cha -f $CENTER_FREQ -r $SAMPLE_RATE $RINGBUFFER_DIR &
+sleep 10
+
+drf ringbuffer -z $RINGBUFFER_SIZE $RINGBUFFER_DIR -p 2 &
+sleep 10
+
+# how many CPU cores to use (do not allocate all cores to this program,
+# leave some for thor.py and ringbuffer.py)
+N_CPUS=4
+# we use 2 cores for each mpi process when calculating an ionogram
+N_CPUS_I=`expr $N_CPUS / 2`
+
+# spawn the program scripts
+mpirun -np $N_CPUS_I python3 calc_ionograms.py $CONF_FILE >logs/calc_ionograms.log 2>&1 &
+python3 find_timings.py $CONF_FILE >logs/find_timings.log 2>&1 &
+mpirun -np $N_CPUS python3 detect_chirps.py $CONF_FILE >logs/detect_chirps.log 2>&1 &
+python3 plot_ionograms.py $CONF_FILE >logs/plot_ionograms.log 2>&1 &
+
+# processes can be killed using:
+#   pkill -f python3
+#   pkill -f drf
+#   rm -rf RINGBUFFER_DIR
